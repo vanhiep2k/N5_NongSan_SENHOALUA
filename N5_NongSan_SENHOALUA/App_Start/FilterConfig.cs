74b70d8d68dc801f5105bf2df4be859418612f1c
NVM23M <vunamisme@gmail.com>
2023-10-14 13:23:44 +0700

    Initial commit

---
+++ N5_NongSan_SENHOALUA/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace N5_NongSan_SENHOALUA
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ N5_NongSan_SENHOALUA/Controllers/NongSanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace N5_NongSan_SENHOALUA.Controllers
+{
+    public class NongSanController : Controller
+    {
+        // GET: NongSan
+        public ActionResult TrangChu()
+        {
+            return View();
+        }
+    }
+}
